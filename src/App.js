09559a52e9c4b11ae24cfd785138587d01976cf2
uzairiqbal <uzair.qq@outlook.com>
2020-09-25 02:54:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [showCounter, setShowCounter] = useState(true);
+
   return (
     <div>
-      <h1> hello world </h1>{" "}
+      <button
+        onClick={() => {
+          setShowCounter(!showCounter);
+        }}
+      >
+        {showCounter ? "Hide Counter" : "Show Counter"}
+      </button>
+
+      {
+        <div style={{ display: showCounter ? "block" : "none" }}>
+          {" "}
+          Counter is : {count}
+        </div>
+      }
+      <br />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        className=""
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
