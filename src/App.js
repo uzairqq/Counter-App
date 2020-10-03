2a382fe828307055e9681f60a8173a7c8cb1b1be
uzairiqbal <uzair.qq@outlook.com>
2020-10-03 16:26:06 +0500

    counter app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
 import React, { useState } from "react";
+import { Card } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   let [count, setCount] = useState(0);
-  let [showCounter, setShowCounter] = useState(true);
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          setShowCounter(!showCounter);
-        }}
-      >
-        {showCounter ? "Hide Counter" : "Show Counter"}
-      </button>
-
-      {
-        <div style={{ display: showCounter ? "block" : "none" }}>
-          {" "}
-          Counter is : {count}
-        </div>
-      }
-      <br />
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
-      <button
-        className=""
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
-      <button
-        onClick={() => {
-          setCount(0);
-        }}
-      >
-        Reset
-      </button>
+    <div className="d-flex vh-100">
+      <div className="d-flex w-100 justify-content-center align-self-center">
+        <Card border="primary" style={{ width: "30rem" }}>
+          <Card.Body>
+            <Card.Title>
+              <b>Counter App</b>
+            </Card.Title>
+            <Card.Text>
+              {"Counter is :" + count}
+              <br />
+              <input type="text"></input>
+            </Card.Text>
+            <Card.Footer>
+              <button
+                className={"btn btn-primary"}
+                onClick={() => {
+                  setCount(count + 1);
+                }}
+              >
+                Increase
+              </button>
+              <button
+                className=""
+                onClick={() => {
+                  setCount(count - 1);
+                }}
+              >
+                Decrease
+              </button>
+              <button
+                onClick={() => {
+                  setCount(0);
+                }}
+              >
+                Reset
+              </button>
+            </Card.Footer>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
